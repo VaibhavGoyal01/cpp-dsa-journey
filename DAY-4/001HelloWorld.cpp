7b54bb23ba24496d5e3146266cd01316b291860d
Vaibhav Goyal <90755274+VaibhavGoyal01@users.noreply.github.com>
2024-08-11 00:04:12 +0530

    Create 001HelloWorld.cpp

---
+++ DAY-4/001HelloWorld.cpp
@@ -0,0 +1,24 @@
+#include<iostream> // preprocessor directive
+
+using namespace std;
+
+int main() {
+
+	/*
+	cout << "namaste";
+	cout << '\n';
+	cout << 5;
+	cout << '\n';
+	cout << '*';
+	cout << endl;
+	cout << 1 + 2;
+	cout << endl;
+	cout << "1+2";
+	cout << endl;
+	cout << 7 * 4;
+	*/
+
+	cout << "Hello World" << endl;
+
+	return 0;
+}
